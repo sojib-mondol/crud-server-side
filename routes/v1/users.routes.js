d0a774dee9add2463a2b9ea22ba7e8e631a671fe
sojib-sudo <mdsojibmandol@gmail.com>
2023-07-10 21:18:07 +0600

    user update api added

--- routes/v1/users.routes.js
+++ routes/v1/users.routes.js
@@ -22,8 +22,6 @@ router.post("/user", async (req, res) => {
   }
 });
 
-
-
 // get api
 router.get("/users", async (req, res) => {
   try {
@@ -39,13 +37,41 @@ router.get("/users", async (req, res) => {
   }
 });
 
+// update the user
+router.put("/update/:id", async (req, res) => {
+  try {
+    const client = db.getClient(); // Use the existing database client
+    const users = client.db("demoCrudAppTask").collection("users");
+    const id = req.params.id;
+    const addUser = req.body;
+    const fields = Object.keys(addUser);
+    const existField = fields.filter(each => addUser[each]);
+    let updateValue = {}
+    existField.forEach(each => {
+      updateValue[each] = addUser[each]
+    })
+    
+    const filter = { _id: new ObjectId(id) };
+    const options = { upsert: true };
+    const updateDoc = {
+      $set: updateValue,
+    };
+    const result = await users.updateOne(filter, updateDoc, options);
+    res.send(result);
+  } catch (error) {
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
 
 // get data by id
 router.get("/user/:id", async (req, res) => {
   try {
     const client = db.getClient(); // Use the existing database client
     const users = client.db("demoCrudAppTask").collection("users");
-    
+
     const id = req.params.id;
     const query = { _id: new ObjectId(id) };
     const result = await users.findOne(query);
@@ -58,8 +84,6 @@ router.get("/user/:id", async (req, res) => {
     });
   }
 });
-
-
 
 // delete a user
 router.delete("/user/:id", async (req, res) => {
